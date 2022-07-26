10efc209012048560ab6e12c051a7f91ef385ca8
CadenaCristian <70227068+CadenaCristian@users.noreply.github.com>
2022-07-26 10:27:37 -0500

    Se acambia la url del cluster

--- calificappBackend/mydb.py
+++ calificappBackend/mydb.py
@@ -1,7 +1,8 @@
 import pymongo
 
+
 cluster = pymongo.MongoClient(
-    'mongodb://admin:admin@calificapp-shard-00-00.bxej7.mongodb.net:27017,calificapp-shard-00-01.bxej7.mongodb.net:27017,calificapp-shard-00-02.bxej7.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-mfbpf9-shard-0&authSource=admin&retryWrites=true&w=majority')
+    "mongodb+srv://cristian:Linda1208@calificapp.bxej7.mongodb.net/?retryWrites=true&w=majority")
 # &connectTimeoutMS=60000
 
 db = cluster["CalificAPP"]
